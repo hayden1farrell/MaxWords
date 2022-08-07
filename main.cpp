61aef04ad908cd5272da1e710c4e151c7656093d
hayden1farrell <aq212306@aquinas.ac.uk>
2022-08-07 17:22:56 +0100

    Reads files, splits words into same ascii val

---
+++ main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+
+std::vector<std::string> ReadFile()
+{
+    std::fstream file;
+    file.open("words/2letters.txt", std::ios::in);
+    if(!file.is_open()){
+        std::cout << "Failed to open file " << std::endl;
+    }
+
+    std::cout << "Opened file" << std::endl;
+    std::vector<std::string> words;
+    std::string line;
+    while(getline(file, line))
+    {
+        size_t pos = 0;
+        bool space = (line.find(" ") != std::string::npos);
+        if(space == false){
+            std::string::iterator end_pos = std::remove(line.begin(), line.end(), ' ');
+            line.erase(end_pos, line.end());
+            words.push_back(line);
+        }
+        else{
+            while (space) {
+                pos = line.find(" ");
+                words.push_back(line.substr(0, pos));
+                line.erase(0, pos + 1);
+                space = (line.find(" ") != std::string::npos);
+            }
+        }
+    }
+    
+    return words;
+}
+
+std::vector<std::string> RemoveDups(std::vector<std::string> words)
+{
+    std::unordered_map<int, std::vector<std::string>> chars;
+    for(auto word: words){
+        int size = 0;
+        for(char letter:word){
+            size += (int)letter;
+        }
+        chars[size].push_back(word);
+    }
+
+    for(auto temp:chars){
+        std::cout << temp.first << ": ";
+        for(auto word:temp.second){
+            std::cout << word << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    return words;
+}
+
+int main()
+{
+    std::vector<std::string> words = ReadFile();
+    std::cout << "Number of words: " << words.size() << std::endl;
+    words = RemoveDups(words);
+
+    return 0;
+}
